e776bfa62b702749d55bf1ee0caa4ea69cbd6027
rfelipesilva <42416868+rfelipesilva@users.noreply.github.com>
2021-04-28 01:45:25 -0300

    Update

---
+++ src/Config.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#! Python3.8
+#? @author: Renan Silva
+#? @github: https://github.com/rfelipesilva
+
+import pandas as pd
+
+class Data:
+    """Class to centralize and provide any kind of data/path/credentials
+    """
+
+    def __init__(self):
+        pass
+    
+    def get_stocks_list(self):
+        
+        stocks_list = pd.read_csv('data/bra_stocks_list.csv', delimiter = '|', encoding = 'utf-8')
+        return list(stocks_list['Symbol'].sort_values(ascending = True))
--- src/app.py
+++ src/app.py
@@ -1,10 +1,26 @@
-# Signture
+# -*- coding: utf-8 -*-
+#! Python3.8
+#? @author: Renan Silva
+#? @github: https://github.com/rfelipesilva
 
 import pandas as pd
 import streamlit as st
 
-st.title('Testing streamlit')
+from Config import Data
+
+st.title('Stock Analyzer')
 
 st.markdown("""
-Trying streamlit markdown feature.
+Stock Analyzer makes comparisons between stocks look easy, give it a try!
 """)
+
+stocks_list = Data().get_stocks_list()
+
+st.sidebar.header('Selection')
+selected_period = st.sidebar.selectbox('Period:', ['1mo', '3mo', '6mo', '1y'])
+
+selected_stocks = st.sidebar.multiselect('Stocks', stocks_list)
+
+#! TO DO:
+#@st.cache
+#def load_data
