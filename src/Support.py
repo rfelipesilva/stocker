a8a721db36597e371aa97ba8d214b4bba24e8c1e
Renan Silva <renan.felipe39@gmail.com>
2021-05-03 18:06:57 -0300

    update

--- src/Support.py
+++ src/Support.py
@@ -15,3 +15,14 @@ class Data:
         
         stocks_list = pd.read_csv('data/bra_stocks_list.csv', delimiter = '|', encoding = 'utf-8')
         return list(stocks_list['Symbol'].sort_values(ascending = True))
+
+    def get_period_dict(self, period_value):
+
+        period_dict = {
+            '1 Month' : '1mo',
+            '3 Months' : '3mo',
+            '6 Months' : '6mo',
+            '1 Year' : '1y'
+        }
+
+        return period_dict[period_value]
