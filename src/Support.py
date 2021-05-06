afbabb152f6bb6695b0ba484cad246e3f0f56796
Renan Silva <renan.felipe39@gmail.com>
2021-05-06 01:18:54 -0300

    dictionary updated with languages support

--- src/Support.py
+++ src/Support.py
@@ -1,6 +1,6 @@
-#! Python3.8
-#? @author: Renan Silva
+# @author: Renan Silva
 #? @github: https://github.com/rfelipesilva
+#! Python3.8
 
 import pandas as pd
 
@@ -16,13 +16,52 @@ class Data:
         stocks_list = pd.read_csv('data/bra_stocks_list.csv', delimiter = '|', encoding = 'utf-8')
         return list(stocks_list['Symbol'].sort_values(ascending = True))
 
-    def get_period_dict(self, period_value):
+    def get_language_dict(self):
 
-        period_dict = {
-            '1 Month' : '1mo',
-            '3 Months' : '3mo',
-            '6 Months' : '6mo',
-            '1 Year' : '1y'
+        language_dict = {
+            'pt' : {
+                'language' : 'pt',
+                'sidebar' : {
+                    'period' : {
+                        'header' : 'Seleção',
+                        'title' : 'Selecione o período:',
+                        'values' : {
+                            '1 Mês' : '1mo',
+                            '3 Meses' : '3mo',
+                            '6 Meses' : '6mo',
+                            '1 Ano' : '1y'
+                        }
+                    },
+                    'stocks' : {
+                        'title' : 'Selecione as ações que deseja comparar:'
+                    }
+                },
+                'body' : {
+                    'description' : 'Stocker faz comparações entre ações parecer fácil, dá uma chance!' 
+                }
+
+            },
+            'en': {
+                'language' : 'en',
+                'sidebar' : {
+                    'period' : {
+                        'header' : 'Selection',
+                        'title' : 'Select period:',
+                        'values' : {
+                            '1 Month' : '1mo',
+                            '3 Months' : '3mo',
+                            '6 Months' : '6mo',
+                            '1 Year' : '1y'
+                        }
+                    },
+                    'stocks' : {
+                        'title' : 'Select the stocks you want to compare:'
+                    }
+                },
+                'body' : {
+                    'description' : 'Stocker makes comparisons between stocks look easy, give it a try!'
+                }
+            }
         }
 
-        return period_dict[period_value]
+        return language_dict
