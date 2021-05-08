1b9653e85b0a201bb1cf98269c8c6277c9ee832d
Renan Silva <renan.felipe39@gmail.com>
2021-05-07 21:39:59 -0300

    some pep8 review

--- src/Support.py
+++ src/Support.py
@@ -9,12 +9,10 @@ class Data:
     """
     
     def get_stocks_list():
-        
         stocks_list = pd.read_csv('data/bra_stocks_list.csv', delimiter = '|', encoding = 'utf-8')
         return list(stocks_list['Symbol'].sort_values(ascending = True))
 
     def get_language_dict():
-
         language_dict = {
             'pt' : {
                 'language' : 'pt',
