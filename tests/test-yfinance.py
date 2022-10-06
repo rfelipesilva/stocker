c92ff772f9ef57cf558a59a556f4747b0b1dc592
Renan Silva <renan.felipe39@gmail.com>
2022-10-05 23:07:52 -0300

    testing new approach to get stock info

--- tests/test-yfinance.py
+++ tests/test-yfinance.py
@@ -4,7 +4,10 @@ from pandas_datareader import data as pdr
 
 stock_code = 'PETR4.SA'
 
-# stock = yf.Ticker(stock_code)
-stock = pdr.DataReader(stock_code, data_source='yahoo', start=f'02-20-2020', end='02-20-2021')
+# stock = yf.Ticker(stock_code) # MM - DD - YYYY
+# stock = pdr.DataReader(stock_code, data_source='yahoo', start=f'10-5-2022', end='10-5-2022')
 
-print(stock)
+# print(stock['Close'])
+
+stock = pdr.DataReader('PETR4.SA', data_source='yahoo', start='10-1-2022', end='10-5-2022')
+print(stock['Close'])
