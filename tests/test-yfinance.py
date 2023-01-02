3f2278692fffaf7ba3f29ca26066560a9d40726d
rfelipesilva <renan.felipe39@gmail.com>
2023-01-02 15:02:27 -0300

    testing with new method

--- tests/test-yfinance.py
+++ tests/test-yfinance.py
@@ -3,6 +3,9 @@
 #! Python3.8
 
 from pandas_datareader import data as pdr
+import yfinance as yf
+
+yf.pdr_override()
 
 stock_code = 'PETR4.SA'
 
@@ -10,6 +13,11 @@ stock_code = 'PETR4.SA'
 # stock = pdr.DataReader(stock_code, data_source='yahoo', start=f'10-5-2022', end='10-5-2022')
 
 # print(stock['Close'])
+# old
+# stock = pdr.DataReader(['PETR4.SA'], data_source='yahoo', start='10-1-2022', end='10-5-2022')
 
-stock = pdr.DataReader('PETR4.SA', data_source='yahoo', start='10-1-2022', end='10-5-2022')
-print(stock['Close'])
+# added yfinance again
+# new
+stock = pdr.get_data_yahoo('ITSA4.SA', start='2022-12-26', end='2022-12-30')
+# print(stock['Close'])
+print(stock)
